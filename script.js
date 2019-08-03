9285fb7c139f58a25eaf3461480a4529817fda84
rileyhoff <rhoff@A-Higher-Power.localdomain>
2019-08-03 15:54:59 -0400

    add Mobile page for store.html

--- script.js
+++ script.js
@@ -2,14 +2,19 @@ function displayItems() {
     var url = document.URL;
     var id = url.substring(url.lastIndexOf('#')+1);
     document.getElementById(id).style.display = "block";
+    document.getElementById(id+"Nav").className = "active";
 }
-function changeItems(id) {
+function changeItems(id, el) {
+    //display main images 
     var x = document.getElementsByTagName("MAIN");
     var i;
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     } 
     document.getElementById(id).style.display = "block";
+    //change active page in navigation
+    document.getElementById("nav").querySelector(".active").className= "";
+    document.getElementById(id+"Nav").className = "active";
 }
 
 function moveTitle() {
